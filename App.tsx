fb1dde32dca03702798bdc1b4e7655b294babeb7
vumanhtu <tu.vumanh@apuscorp.com>
2023-08-09 15:56:51 +0700

    refactor home

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -8,6 +9,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -19,17 +21,17 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
+  title2: string;
+  with2: boolean;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({children, title, title2, with2}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -41,11 +43,13 @@ function Section({children, title}: SectionProps): JSX.Element {
           },
         ]}>
         {title}
+        <Text style={[styles.highlight]}>{title2}</Text>
       </Text>
       <Text
         style={[
           styles.sectionDescription,
           {
+            width: with2 ? 200 : 300,
             color: isDarkMode ? Colors.light : Colors.dark,
           },
         ]}>
@@ -76,17 +80,24 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <View style={styles.rowContainer}>
+            <Section title="Hello i'm" title2=" Vu Manh Tu" with2>
+              Tôi là{' '}
+              <Text style={styles.highlightDescription}>developer mobile </Text>
+              welcome to the app me.
+            </Section>
+            <Image source={require('./image/aeon.jpeg')} style={styles.image} />
+          </View>
+          <Section title="I come from" title2=" Tuyen Quang" with2={true}>
+            <Text>I love here</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="favorite sayings" title2="" with2={false}>
+            <Text style={(styles.highlightDescription, {color: '#f0f'})}>
+              Genius is one percent inspiration and ninety-nine percent
+              perspiration.
+            </Text>
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
+          <Section title="Learn More" title2="" with2={false}>
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
@@ -97,6 +108,12 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  rowContainer: {
+    flexDirection: 'row', // Set flexDirection to 'row' for horizontal layout
+    justifyContent: 'center', // Center items horizontally within the row
+    alignItems: 'center', // Center items vertically within the row
+    height: 100, // Set a fixed height for the row
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -104,14 +121,28 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    width: 250,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    width: 200,
   },
   highlight: {
+    fontWeight: '600',
+    color: '#8DEEEE',
+  },
+  highlightDescription: {
     fontWeight: '700',
+    color: '#FF0000',
+  },
+  image: {
+    top: 40,
+    right: 20,
+    width: 100,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
 
